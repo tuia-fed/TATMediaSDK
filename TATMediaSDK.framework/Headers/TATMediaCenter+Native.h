71e69af208ecd2de4bb49a92be78c0e263367cc4
xuesong-iOS <wurui1@tuia.cn>
2020-03-20 18:59:25 +0800

    1.1.1.2

--- TATMediaSDK.framework/Headers/TATMediaCenter+Native.h
+++ TATMediaSDK.framework/Headers/TATMediaCenter+Native.h
@@ -11,11 +11,6 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-typedef NS_ENUM(NSUInteger, TATNativeAdType){
-    TATNativeAdTypeInterstitial, // 插屏式
-    TATNativeAdTypeEmbed, // 嵌入式
-};
-
 @class TATBaseAdView;
 
 @interface TATMediaCenter (Native)
